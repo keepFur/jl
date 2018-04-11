cf50feabeeafe02887cbf7995b8b5b9c80265df5
keepFur <keepFur@163.com>
2018-04-11 08:14:07 +0800

    工作经验和项目经理已完成

--- src/views/ProjectExprience.vue
+++ src/views/ProjectExprience.vue
@@ -2,7 +2,6 @@
 .container {
     background: #eee;
     padding: 20px;
-    /* margin-top: 60px; */
 }
 
 .time {
@@ -19,25 +18,61 @@
         <Card :bordered="false">
             <p slot="title">项目经验</p>
             <Timeline>
-                <TimelineItem>
-                    <p class="time">1976年</p>
-                    <p class="content">Apple I 问世</p>
-                </TimelineItem>
-                <TimelineItem>
-                    <p class="time">1984年</p>
-                    <p class="content">发布 Macintosh</p>
-                </TimelineItem>
-                <TimelineItem>
-                    <p class="time">2007年</p>
-                    <p class="content">发布 iPhone</p>
-                </TimelineItem>
-                <TimelineItem>
-                    <p class="time">2010年</p>
-                    <p class="content">发布 iPad</p>
-                </TimelineItem>
-                <TimelineItem>
-                    <p class="time">2011年10月5日</p>
-                    <p class="content">史蒂夫·乔布斯去世</p>
+                <TimelineItem v-for="(project ,index) in projects" :key="index">
+                    <p class="time">
+                        <span v-text="project.date"></span>
+                        <a href="#" slot="extra" @click.prevent="detailHandle(index)">
+                            <Icon type="eye"></Icon>
+                            详情
+                        </a>
+                        <a href="#" slot="extra" @click.prevent="editHandle(index)">
+                            <Icon type="edit"></Icon>
+                            编辑
+                        </a>
+                    </p>
+                    <div class="content" v-if="!project.isEdit">
+                        <p>
+                            <span solt="title" v-show="project.isDetail">名称：</span>
+                            <span v-text="project.name"></span>
+                        </p>
+                        <p v-show="project.isDetail">
+                            <span solt="title">角色：</span>
+                            <span v-text="project.role"></span>
+                        </p>
+                        <p v-show="project.isDetail">
+                            <span slot="title">描述：</span>
+                            <span v-text="project.description"></span>
+                        </p>
+                        <p v-show="project.isDetail">
+                            <span slot="title">业绩：</span>
+                            <span v-text="project.achievement"></span>
+                        </p>
+                    </div>
+                    <div class="content" v-if="project.isEdit">
+                        <Form :model="project" :label-width="80">
+                            <FormItem label="名称">
+                                <Input v-model="project.name"></Input>
+                            </FormItem>
+                            <FormItem label="角色">
+                                <Select v-model="project.role">
+                                    <Option v-for="role in roles" :value="role.value" :key="role.key" :label="role.value">{{ role.value }}</Option>
+                                </Select>
+                            </FormItem>
+                            <FormItem label="描述">
+                                <Input v-model="project.description" type='textarea' :autosize="{minRows: 4,maxRows: 6}" placeholder="请详细的描述项目"></Input>
+                            </FormItem>
+                            <FormItem label="业绩">
+                                <Input v-model="project.achievement" type="textarea" :autosize="{minRows: 4,maxRows: 6}" placeholder="请说说这个项目为公司带来的收益"></Input>
+                            </FormItem>
+                            <FormItem label="时间段">
+                                <Input v-model="project.date" placeholder="eg：2017年10月至2018年12月"></Input>
+                            </FormItem>
+                            <FormItem>
+                                <Button type="primary" @click="saveEditInfoHandle(index)">保存</Button>
+                                <Button type="ghost" style="margin-left: 8px" @click="cancleEditHandle(index)">取消</Button>
+                            </FormItem>
+                        </Form>
+                    </div>
                 </TimelineItem>
             </Timeline>
         </Card>
@@ -45,6 +80,95 @@
 </template>
 <script>
 export default {
+    data: function() {
+        return {
+            cacheKey: 'projectsExprience',
+            roles: [
+                {
+                    key: 1,
+                    value: '开发'
+                }, {
+                    key: 4,
+                    value: '测试'
+                }, {
+                    key: 2,
+                    value: '项目经理'
+                }, {
+                    key: 3,
+                    value: '技术经理'
+                }, {
+                    key: 5,
+                    value: '产品经理'
+                }
+            ],
+            projects: [{
+                isEdit: false,
+                isDetail: false,
+                name: '亚马逊客服管理系统',
+                url: 'www.acs.aukeyit.com',
+                role: '开发',
+                description: '亚马逊客服管理系统是一个管理系统',
+                achievement: '为公司节省了大量的人力资源和软件成本',
+                beginDate: '2017年10月',
+                endDate: '2017年12月',
+                date: '2017年10月至2017年12月'
+            }, {
+                isEdit: false,
+                isDetail: false,
+                name: '亚马逊客服管理系统',
+                url: 'www.acs.aukeyit.com',
+                role: '开发',
+                description: '亚马逊客服管理系统是一个管理系统',
+                achievement: '为公司节省了大量的人力资源和软件成本',
+                beginDate: '2017年10月',
+                endDate: '2017年12月',
+                date: '2017年10月至2017年12月'
+            }]
+        };
+    },
+    // 创建完成之后
+    created: function() {
+        if (this.getInfo()) {
+            this.projects = this.getInfo();
+        }
+    },
+    methods: {
+        // 存到缓存中去
+        setInfo: function() {
+            window.localStorage.setItem(this.cacheKey, JSON.stringify(this.projects));
+        },
+        // 从缓存中取数据
+        getInfo: function() {
+            return JSON.parse(window.localStorage.getItem(this.cacheKey));
+        },
+        // 设置所有的项目经验都不能编辑
+        setAllDisabledEdit:function(){
+             this.projects  = this.projects.map(function(item){
+                 item.isEdit = false;
+                 return item;
+             });
+        },
+        // 详情
+        detailHandle: function(index) {
+            this.projects[index].isDetail = !this.projects[index].isDetail;
+        },
+        // 编辑
+        editHandle: function(index) {
+            this.setAllDisabledEdit();
+            this.projects[index].isEdit = !this.projects[index].isEdit;
+        },
+        // 保存
+        saveEditInfoHandle: function(index) {
+            this.projects[index].isEdit = false;
+            this.projects[index].isDetail = true;
+            this.setInfo();
+        },
+        // 取消
+        cancleEditHandle: function(index) {
+            this.projects[index].isEdit = false;
+            this.projects = this.getInfo();
+        }
+    }
 
 }
 </script>
--- src/views/WorkExperience.vue
+++ src/views/WorkExperience.vue
@@ -15,50 +15,220 @@
 </style>
 <template>
     <div class="container">
-        <Card :bordered="false" v-if="!isEdit">
+        <Card :bordered="false">
             <p slot="title">工作经历</p>
-            <a href="#" slot="extra" @click.prevent.stop="editSkillsHandle">
-                <Icon type="edit"></Icon>
-                编辑
+            <a href="#" slot="extra" @click.prevent.stop="addInfoHandle">
+                <Icon type="plus-circled"></Icon>
+                添加
             </a>
             <Timeline>
-                <TimelineItem>
-                    <p class="time">1976年</p>
-                    <p class="content">Apple I 问世</p>
-                </TimelineItem>
-                <TimelineItem>
-                    <p class="time">1984年</p>
-                    <p class="content">发布 Macintosh</p>
-                </TimelineItem>
-                <TimelineItem>
-                    <p class="time">2007年</p>
-                    <p class="content">发布 iPhone</p>
-                </TimelineItem>
-                <TimelineItem>
-                    <p class="time">2010年</p>
-                    <p class="content">发布 iPad</p>
-                </TimelineItem>
-                <TimelineItem>
-                    <p class="time">2011年10月5日</p>
-                    <p class="content">史蒂夫·乔布斯去世</p>
+                <TimelineItem v-for="(work ,index) in works" :key="index">
+                    <p class="time">
+                        <span v-text="work.date"></span>
+                        <a href="#" slot="extra" @click.prevent="detailHandle(index)">
+                            <Icon type="eye"></Icon>
+                            详情
+                        </a>
+                        <a href="#" slot="extra" @click.prevent="editHandle(index)">
+                            <Icon type="edit"></Icon>
+                            编辑
+                        </a>
+                    </p>
+                    <div class="content" v-if="!work.isEdit">
+                        <p v-show="!work.isDetail">
+                            {{work.companyName+'（'+work.jobName+'）'}}
+                        </p>
+                        <p v-show="work.isDetail">
+                            <span solt="title">职位：</span>
+                            <span v-text="work.jobName"></span>
+                        </p>
+                        <p v-show="work.isDetail">
+                            <span solt="title">公司名称：</span>
+                            <span v-text="work.companyName"></span>
+                        </p>
+                        <p v-show="work.isDetail">
+                            <span solt="title">公司类型：</span>
+                            <span v-text="work.companyType"></span>
+                        </p>
+                        <p v-show="work.isDetail">
+                            <span slot="title">工作内容：</span>
+                            <span v-text="work.content"></span>
+                        </p>
+                        <p v-show="work.isDetail">
+                            <span slot="title">业绩：</span>
+                            <span v-text="work.achievement"></span>
+                        </p>
+                    </div>
+                    <div class="content" v-if="work.isEdit">
+                        <Form :model="work" :label-width="80">
+                            <FormItem label="职位">
+                                <Input v-model="work.jobName"></Input>
+                            </FormItem>
+                            <FormItem label="公司名称">
+                                <Input v-model="work.companyName"></Input>
+                            </FormItem>
+                            <FormItem label="公司类型">
+                                <Input v-model="work.companyType" placeholder="eg:电子商务"></Input>
+                            </FormItem>
+                            <FormItem label="业绩">
+                                <Input v-model="work.achievement" type="textarea" :autosize="{minRows: 4,maxRows: 6}" placeholder="选填，请说说你在就职期间为公司作出的贡献"></Input>
+                            </FormItem>
+                            <FormItem label="时间段">
+                                <Input v-model="work.date" placeholder="eg：2017年10月至2018年12月"></Input>
+                            </FormItem>
+                            <FormItem>
+                                <Button type="primary" @click="saveEditInfoHandle(index)">保存</Button>
+                                <Button type="ghost" style="margin-left: 8px" @click="cancleEditHandle(index)">取消</Button>
+                            </FormItem>
+                        </Form>
+                    </div>
                 </TimelineItem>
             </Timeline>
         </Card>
-        <Card v-if="isEdit">
-
-        </Card>
+        <Modal v-model="addModal" title="添加工作经历" @on-ok="saveAddInfoHandle" @on-cancel="cancleAddHandle">
+            <Form :model="formItem" :label-width="80">
+                <FormItem label="职位">
+                    <Input v-model="formItem.jobName"></Input>
+                </FormItem>
+                <FormItem label="公司名称">
+                    <Input v-model="formItem.companyName"></Input>
+                </FormItem>
+                <FormItem label="公司类型">
+                    <Input v-model="formItem.companyType" placeholder="eg:电子商务"></Input>
+                </FormItem>
+                <FormItem label="业绩">
+                    <Input v-model="formItem.achievement" type="textarea" :autosize="{minRows: 4,maxRows: 6}" placeholder="选填，请说说你在就职期间为公司作出的贡献"></Input>
+                </FormItem>
+                <FormItem label="时间段">
+                    <Input v-model="formItem.date" placeholder="eg：2017年10月至2018年12月"></Input>
+                </FormItem>
+            </Form>
+        </Modal>
     </div>
 </template>
 <script>
 export default {
     data: function() {
         return {
-            isEdit: false
+            validRules: {
+                jobName: '工作名称不能为空',
+                companyName: '公司名称不能为空',
+                companyType: '公司类型不能为空',
+                content: '工作内容不能为空',
+                date: '工作时间段不能为空'
+            },
+            cacheKey: 'workExprience',
+            addModal: false,
+            formItem: {
+                isDetail: true,
+                isEdit: false,
+                jobName: '',
+                companyName: '',
+                companyType: '',
+                skills: '',
+                content: '',
+                achievement: '',
+                date: ''
+            },
+            works: [{
+                isDetail: false,
+                isEdit: false,
+                jobName: 'web前端开发',
+                companyName: '深圳市傲基电子商务股份有限公司',
+                companyType: '电子商务',
+                skills: ['js/html/css', 'jQeruy', 'bootstrap', 'node.js'],
+                content: '开发供应链系统',
+                achievement: '公司的整条供应链系统被打通',
+                date: '2016年3月至现在'
+            }]
         };
     },
+    // 创建完成之后
+    created: function() {
+        this.works = this.getInfo();
+    },
     methods: {
-        editSkillsHandle: function() {
-
+        // 存到缓存中去
+        setInfo: function() {
+            window.localStorage.setItem(this.cacheKey, JSON.stringify(this.works));
+        },
+        // 从缓存中取数据
+        getInfo: function() {
+            return JSON.parse(window.localStorage.getItem(this.cacheKey)) || this.works;
+        },
+        // 设置所有的项目经验都不能编辑
+        setAllDisabledEdit: function() {
+            this.works = this.works.map(function(item) {
+                item.isEdit = false;
+                return item;
+            });
+        },
+        // 添加
+        addInfoHandle: function() {
+            this.addModal = true;
+        },
+        // 验证表单添加的信息
+        validateForm: function(formData, callback) {
+            let isPass = true;
+            for (var key in formData) {
+                if (formData.hasOwnProperty(key)) {
+                    var val = formData[key];
+                    if (typeof val === 'string' && !val.replace(/^\s+|\s+$/, '')) {
+                        isPass = false;
+                        callback(this.validRules[key]);
+                        break;
+                    }
+                }
+            }
+            if (isPass) {
+                callback();
+            }
+        },
+        // 保存添加的信息
+        saveAddInfoHandle: function() {
+            var that = this;
+            this.validateForm.call(this, this.formItem, function(err) {
+                if (err) {
+                    this.$Notice.error({
+                        title: err
+                    });
+                    return;
+                }
+                this.works.push(thatformItem);
+            });
+            // this.validateForm(this.formItem, function(err) {
+            //     if (err) {
+            //         that.$Notice.error({
+            //             title: err
+            //         });
+            //         return;
+            //     }
+            //     that.works.push(thatformItem);
+            // });
+        },
+        // 取消添加
+        cancleAddHandle: function() {
+            this.addModal = false;
+        },
+        // 详情
+        detailHandle: function(index) {
+            this.works[index].isDetail = !this.works[index].isDetail;
+        },
+        // 编辑
+        editHandle: function(index) {
+            this.setAllDisabledEdit();
+            this.works[index].isEdit = !this.works[index].isEdit;
+        },
+        // 保存
+        saveEditInfoHandle: function(index) {
+            this.works[index].isEdit = false;
+            this.works[index].isDetail = true;
+            this.setInfo();
+        },
+        // 取消
+        cancleEditHandle: function(index) {
+            this.works[index].isEdit = false;
+            this.works = this.getInfo();
         }
     }
 }
